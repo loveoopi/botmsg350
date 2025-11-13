1fbd7cb2562e1d511ebf1dd6e1e9ffee5d925b9e
loveoopi <143441383+loveoopi@users.noreply.github.com>
2025-11-13 16:10:46 +0530

    Increase DELETE_DELAY from 5 to 290 seconds

--- config.py
+++ config.py
@@ -7,7 +7,7 @@ BOT_TOKEN = "8315509362:AAEJhfPfiyLz8y241Na6giOVulrHfZzjPIo"
 SESSION_STRING = "1AZWarzgBu2JOqUONinrdoE_xtSaglY6J2r84dUtTd-Odw3BY64qNRXqGhh8RDEZRs0o8JmsOjs47_nkr9lu2wLViF-O3lWsPadM6bD8prGtK1poQq0fteMHOBFGfzn6HpYEckDAiY3leO5wQTzMwz94LfZ-lGwXxrFhNs8jUwnIWCcHaV1Cbq7yUZ5Xec6r3R1C3ID-KVb_b_KK4tQ1L0vvBwL3nRusBDhgbiJxJKPKTk-zVEtk71kJUJ7FnvjLNGtTZw516FU9QXB67RTmw5TaDxS7h-Mga4xG8WxB7qou6bUiayuMHT71lFKOtl7u2BVy_Q5sdAgyV2u69Kte_QuovBlJrDi0="
 
 # Deletion delay in seconds
-DELETE_DELAY = 5
+DELETE_DELAY = 290
 
 # Heroku configuration
 PORT = int(os.environ.get('PORT', 8080))
